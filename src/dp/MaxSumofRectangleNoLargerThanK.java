89a6a33640df31a75b77ca9a7b6066376c178539
hanjg <hanjg>
2021-10-19 23:12:19 +0800

    最大间距

--- src/dp/MaxSumofRectangleNoLargerThanK.java
+++ src/dp/MaxSumofRectangleNoLargerThanK.java
@@ -2,33 +2,47 @@ package dp;
 
 import java.util.*;
 
+/**
+ * problems-363 https://leetcode-cn.com/problems/max-sum-of-rectangle-no-larger-than-k/
+ */
 public class MaxSumofRectangleNoLargerThanK {
+
     public int maxSumSubmatrix(int[][] matrix, int k) {
-        int rows=matrix.length;if(rows==0)return 0;
-        int cols=matrix[0].length;if(cols==0)return 0;
-        int res=Integer.MIN_VALUE;
-        for(int j1=0;j1<cols;j1++){
-        	int[] sums=new int[rows];//记录列[j1,j2]每行的和
-        	for(int j2=j1;j2<cols;j2++){
-        		for(int i=0;i<rows;i++){
-        			sums[i]+=matrix[i][j2];
-        		}
-        		TreeSet<Integer> set=new TreeSet<>();//记录之前每一个[0,i][j1,j2]的和
-        		int val=0;//记录当前[0,i][j1,j2]的和
-        		for(int i=0;i<rows;i++){
-        			val+=sums[i];
-        			if(val<=k)res=Math.max(res, val);
-        			Integer temp=set.ceiling(val-k);//之前[0,x]的和
-        			if(temp!=null)res=Math.max(res, val-temp);//val-temp（两行之间的和）<=k的最大值
-        			set.add(val);
-        		}
-        	}
+        int rows = matrix.length;
+        if (rows == 0) {
+            return 0;
+        }
+        int cols = matrix[0].length;
+        if (cols == 0) {
+            return 0;
+        }
+        int res = Integer.MIN_VALUE;
+        for (int l = 0; l < cols; l++) {
+            int[] rowSums = new int[rows];//记录列[j1,j2]每行的和
+            for (int r = l; r < cols; r++) {
+                for (int i = 0; i < rows; i++) {
+                    rowSums[i] += matrix[i][r];
+                }
+                TreeSet<Integer> set = new TreeSet<>();//记录之前每一个[0,i][j1,j2]的和
+                int val = 0;//记录当前[0,i][j1,j2]的和
+                for (int i = 0; i < rows; i++) {
+                    val += rowSums[i];
+                    if (val <= k) {
+                        res = Math.max(res, val);
+                    }
+                    Integer temp = set.ceiling(val - k);//之前[0,x]的和
+                    if (temp != null) {
+                        res = Math.max(res, val - temp);//val-temp（两行之间的和）<=k的最大值
+                    }
+                    set.add(val);
+                }
+            }
         }
         return res;
     }
 
-	public static void main(String[] args) {
-		int[][] matrix={{5,-4,5},{1,-4,-4},{5,4,-3},{-3,5,-4}};
-		System.out.println(new MaxSumofRectangleNoLargerThanK().maxSumSubmatrix(matrix, 8));
-	}
+    public static void main(String[] args) {
+        int[][] matrix = {{5, -4, 5}, {1, -4, -4}, {5, 4, -3}, {-3, 5, -4}};
+        System.out.println(new MaxSumofRectangleNoLargerThanK().maxSumSubmatrix(matrix, 8));
+    }
 }
--- src/sort/MaximumGap.java
+++ src/sort/MaximumGap.java
@@ -1,35 +1,65 @@
 package sort;
 
-import java.util.Arrays;
-
+/**
+ * problems-164 https://leetcode-cn.com/problems/maximum-gap/
+ */
 public class MaximumGap {
+
     public int maximumGap(int[] nums) {//桶排序思想
-    	if(nums==null||nums.length<2)return 0;
-        int max=nums[0],min=nums[0],n=nums.length;
-        for(int i=1;i<n;i++){
-        	max=Math.max(max, nums[i]);
-        	min=Math.min(min, nums[i]);
+        if (nums == null || nums.length < 2) {
+            return 0;
         }
-        int range=(int)Math.ceil((double)(max-min)/(n-1));
-        if(range==0)return 0;//所有数相等
-        //每个桶的区间长度range,桶的个数为n-1
-        //最大gap一定出现在两个不同的桶中的元素之间
-        int[] buMin=new int[n-1],buMax=new int[n-1];//记录每个桶中的最大和最小元素
-        Arrays.fill(buMin, Integer.MAX_VALUE);
-        Arrays.fill(buMax, Integer.MIN_VALUE);
-        for(int num:nums){
-        	if(num==max)continue;//num为max时id可能越界
-        	int id=(num-min)/range;//num所处桶的序号
-        	buMin[id]=Math.min(buMin[id], num);
-        	buMax[id]=Math.max(buMax[id], num);
+        int n = nums.length;
+        int maxNum = nums[0];
+        int minNum = nums[0];
+        for (int i = 1; i < n; i++) {
+            maxNum = Math.max(maxNum, nums[i]);
+            minNum = Math.min(minNum, nums[i]);
         }
-        int previous=buMax[0],maxGap=previous-min;
-        for(int i=1;i<n-1;i++){
-        	if(buMax[i]==Integer.MIN_VALUE&&buMin[i]==Integer.MAX_VALUE)continue;
-        	maxGap=Math.max(maxGap, buMin[i]-previous);
-        	previous=buMax[i];
+        //所有数都一样，无间隔
+        if (maxNum == minNum) {
+            return 0;
         }
-        maxGap=Math.max(maxGap, max-previous);//max单独考虑
+
+        //总共分n个桶，宽度width，最后一个桶
+        int width = (int) Math.ceil((double) (maxNum - minNum) / (n - 1));
+        //edges[i][0]:i桶里的数最小值，edges[i][1]：i桶里的数最大值
+        int[][] edges = new int[n][2];
+        for (int i = 0; i < edges.length; i++) {
+            for (int j = 0; j < edges[0].length; j++) {
+                edges[i][j] = -1;
+            }
+        }
+
+        for (int num : nums) {
+            int index = (num - minNum) / width;
+            if (edges[index][0] == -1) {
+                edges[index][0] = num;
+            }
+            if (edges[index][1] == -1) {
+                edges[index][1] = num;
+            }
+            edges[index][0] = Math.min(edges[index][0], num);
+            edges[index][1] = Math.max(edges[index][1], num);
+        }
+        int maxGap = 0;
+        int preMax = -1;
+        for (int[] edge : edges) {
+            //和前一个桶比较
+            if (preMax != -1) {
+                maxGap = Math.max(maxGap, edge[0] - preMax);
+            }
+            //桶内部比较
+            maxGap = Math.max(maxGap, edge[1] - edge[0]);
+            if (edge[1] != -1) {
+                preMax = edge[1];
+            }
+        }
         return maxGap;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaximumGap().maximumGap(new int[]{3, 6, 9, 1}));
+        System.out.println(new MaximumGap().maximumGap(new int[]{1, 1000000}));
     }
 }
