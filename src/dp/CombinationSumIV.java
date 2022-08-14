bee6dcb1e92870f94dba49aa63dacd2b34bc2dcc
hanjg <hanjg>
2022-08-14 17:29:53 +0800

    组合总和 Ⅳ

--- src/dp/CombinationSumIV.java
+++ src/dp/CombinationSumIV.java
@@ -2,18 +2,27 @@ package dp;
 
 import java.util.Arrays;
 
+/**
+ * https://leetcode.cn/problems/combination-sum-iv/
+ */
 public class CombinationSumIV {
+
     public int combinationSum4(int[] nums, int target) {
-        int[] sol=new int[target+1];//目标位i的解的个数为sol[i]
-        sol[0]=1;
-        Arrays.sort(nums);
-        for(int i=1;i<=target;i++){
-        	for(int j=0;j<nums.length;j++){
-        		if(nums[j]>i)break;
-        		sol[i]+=sol[i-nums[j]];//以nums[j]为结尾,当nums[j]==i，集合中只存在nums[j]
-        	}
+        int n = nums.length;
+        //dp[i]: 目标和是i的组合数量
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            //考虑集合中最后一个数字
+            for (int last : nums) {
+                if (last > i) {
+                    continue;
+                }
+                //最后一个数字是last
+                dp[i] += dp[i - last];
+            }
         }
-        return sol[target];
+        return dp[target];
     }
 
 }
--- src/weekly/Week1.java
+++ src/weekly/Week1.java
@@ -1,43 +1,52 @@
 package weekly;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author anxi
  * @version 2022/5/8 10:44
  */
 public class Week1 {
 
-    public int rearrangeCharacters(String s, String target) {
-        int[] src = new int[26];
-        for (char c : s.toCharArray()) {
-            src[c - 'a']++;
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return false;
         }
-        int[] tar = new int[26];
-        for (char c : target.toCharArray()) {
-            tar[c - 'a']++;
+        if (n == 2) {
+            return equal2(nums, 1);
         }
-        int res = 1;
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (tar[c - 'a'] > src[c - 'a']) {
-                return 0;
-            }
-            res *= cmn(tar[c - 'a'], src[c - 'a']);
+
+        boolean[] dp = new boolean[n];//nums[0,i)
+        dp[1] = equal2(nums, 1);
+        dp[2] = equal3(nums, 2) || incr(nums, 2);
+        for (int i = 3; i < n; i++) {
+            dp[i] |= equal2(nums, i) && dp[i - 2];
+            dp[i] |= equal3(nums, i) && dp[i - 3];
+            dp[i] |= incr(nums, i) && dp[i - 3];
         }
-        return res;
+        return dp[n - 1];
     }
 
-    private int cmn(int m, int n) {
-        int sum1 = 1;
-        for (int i = 1; i <= m; i++) {
-            sum1 = sum1 * i;
-        }
-        int sum2 = 1;
-        for (int j = n; j >= n - m + 1; j--) {
-            sum2 = sum2 * j;
-        }
-        return sum2 / sum1;
+    private boolean equal3(int[] nums, int i) {
+        return nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2];
+    }
+
+    private boolean equal2(int[] nums, int i) {
+        return nums[i] == nums[i - 1];
+    }
+
+    private boolean incr(int[] nums, int i) {
+        return nums[i] == 1 + nums[i - 1] && nums[i - 1] == 1 + nums[i - 2];
     }
 
     public static void main(String[] args) {
-        System.out.println(new Week1().rearrangeCharacters("ilovecodingonleetcode", "code"));
+        System.out.println(new Week1().validPartition(new int[]{10, 20, 30}));
     }
 }
