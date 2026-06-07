e2c0274cb0d5a3f78cfed7131c61f6f3cc92c261
hanjg <409017803@qq.com>
2026-06-07 15:00:41 +0800

    Refactor node creation in Main class for binary tree to use computeIfAbsent

--- src/test/_260607/Main.java
+++ src/test/_260607/Main.java
@@ -1,3 +1,5 @@
+package test._260607;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
---
+++ src/test/_260609/Main.java
@@ -0,0 +1,7 @@
+package test._260609;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
