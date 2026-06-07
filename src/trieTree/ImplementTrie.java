dc14bd5bc3eeeffd71820bbc3b459602b30525ed
hanjg <409017803@qq.com>
2026-06-07 11:31:53 +0800

    Refactor ImplementTrie for improved readability and performance

--- src/trieTree/ImplementTrie.java
+++ src/trieTree/ImplementTrie.java
@@ -14,67 +14,57 @@ public class ImplementTrie {
     public void insert(String word) {
         TrieNode parentNode = root;
         for (char c : word.toCharArray()) {
-            TrieNode curNode = parentNode.getChild(c);
-            if (curNode == null) {
-                curNode = new TrieNode(c);
-                parentNode.addChild(c, curNode);
+            TrieNode currentNode = parentNode.children[c - 'a'];
+            if (currentNode == null) {
+                currentNode = new TrieNode(c);
+                parentNode.children[c - 'a'] = currentNode;
             }
-            parentNode = curNode;
+            parentNode = currentNode;
         }
-        parentNode.setEnd();
+        parentNode.isWord = true;
     }
 
     public boolean search(String word) {
-        TrieNode node = searchPrefix(word, root);
-        return node != null && node.isEnd();
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isWord;
     }
 
     public boolean startsWith(String prefix) {
-        TrieNode node = searchPrefix(prefix, root);
-        return node != null;
+        return searchPrefix(prefix) != null;
     }
 
-    private TrieNode searchPrefix(String prefix, TrieNode parentNode) {
-        TrieNode curNode = null;
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode cur = root;
         for (char c : prefix.toCharArray()) {
-            curNode = parentNode.getChild(c);
-            if (curNode == null) {
+            TrieNode next = cur.children[c - 'a'];
+            if (next == null) {
                 return null;
             }
-            parentNode = curNode;
+            cur = next;
         }
-        return curNode;
+        return cur;
     }
 
-    class TrieNode {
+    private static class TrieNode {
+        char c;
+        boolean isWord;
+        TrieNode[] children;
 
-        private char val;
-        private boolean isEnd;
-        private TrieNode[] children;
-
-        public TrieNode(char val) {
-            this.val = val;
+        public TrieNode(char c) {
+            this.c = c;
+            this.isWord = false;
             this.children = new TrieNode[26];
         }
-
-        public void addChild(char c, TrieNode child) {
-            this.children[c - 'a'] = child;
-        }
-
-        public TrieNode getChild(char c) {
-            return this.children[c - 'a'];
-        }
-
-        public void setEnd() {
-            this.isEnd = true;
-        }
-
-        public void unSetEnd() {
-            this.isEnd = false;
-        }
-
-        public boolean isEnd() {
-            return this.isEnd;
-        }
     }
+
+    public static void main(String[] args) {
+        ImplementTrie trie = new ImplementTrie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));   // 返回 true
+        System.out.println(trie.search("app"));     // 返回 false
+        System.out.println(trie.startsWith("app")); // 返回 true
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // 返回 true
+    }
+
 }
